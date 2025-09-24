151284159330dde529256c03ea73162689080fcf
NMANISH9800 <96011091+NMANISH9800@users.noreply.github.com>
2025-09-24 21:13:11 +0530

    Delete multivariate directory

--- multivariate/label/lofski.sh
+++
@@ -1,1 +0,0 @@
-python ./scripts/run_benchmark.py --config-path "unfixed_detect_label_multi_config.json" --model-name "tods.lofski" --model-hyper-params '{}' --gpus 0 --num-workers 1 --timeout 60000 --save-path "label/lofski"
